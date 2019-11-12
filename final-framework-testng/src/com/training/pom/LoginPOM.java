14c74f29603c7806f3bbe90dccf479c716bc5090
YEPURIRAO <YEPURIRAO@9.85.114.102>
2019-11-12 17:19:21 +0530

    Added some codes

--- final-framework-testng/src/com/training/pom/LoginPOM.java
+++ final-framework-testng/src/com/training/pom/LoginPOM.java
@@ -1,7 +1,9 @@
 package com.training.pom;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -13,15 +15,40 @@ public class LoginPOM {
 		PageFactory.initElements(driver, this);
 	}
 	
-	@FindBy(id="email")
+	@FindBy(id="login")
 	private WebElement userName; 
 	
-	@FindBy(id="passd")
+	@FindBy(id="password")
 	private WebElement password;
 	
-	@FindBy(id="send2")
+	@FindBy(xpath="//*[@id='form-login_submitAuth']")
 	private WebElement loginBtn; 
 	
+	@FindBy(linkText="My certificates")
+	private WebElement Mycertificates; 
+	
+	@FindBy(linkText="Inbox")
+	private WebElement InboxLink;
+	
+	@FindBy(xpath="//*[@id='navbar']/ul[2]/li[2]/a/span[2]")
+	private WebElement Usericon;
+	@FindBy(xpath="//*[@id='navbar']/ul[2]/li[2]/a/span[2]")
+	private WebElement UsericonClick;
+	
+	@FindBy(id="logout_button")
+	private WebElement logout;
+	
+	
+	
+	
+	public void moveToUserIcon(WebElement Usericon, WebElement UsericonClick)
+	{
+		this.Usericon=Usericon;
+		Actions a= new Actions(driver);
+		a.moveToElement(Usericon).build().perform();
+		driver.findElement((By) this.UsericonClick).click();
+		
+	}
 	public void sendUserName(String userName) {
 		this.userName.clear();
 		this.userName.sendKeys(userName);
--- final-framework-testng/tests/com/training/sanity/tests/ELTC_016.java
+++ final-framework-testng/tests/com/training/sanity/tests/ELTC_016.java
@@ -4,12 +4,16 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.training.generics.GenericMethods;
 import com.training.generics.ScreenShot;
 import com.training.pom.LoginPOM;
 import com.training.utility.DriverFactory;
@@ -17,11 +21,14 @@ import com.training.utility.DriverNames;
 
 public class ELTC_016 {
 
+	private static final WebElement UsericonClick = null;
+	private static final String By = null;
 	private WebDriver driver;
 	private String baseUrl;
 	private LoginPOM loginPOM;
 	private static Properties properties;
 	private ScreenShot screenShot;
+	private WebElement Usericon;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws IOException {
@@ -40,16 +47,26 @@ public class ELTC_016 {
 		driver.get(baseUrl);
 	}
 	
-	@AfterMethod
-	public void tearDown() throws Exception {
-		Thread.sleep(1000);
-		driver.quit();
-	}
+//	@AfterMethod
+//	public void tearDown() throws Exception {
+//		Thread.sleep(1000);
+//		driver.quit();
+//	}
 	@Test
 	public void validLoginTest() {
-		loginPOM.sendUserName("admin");
-		loginPOM.sendPassword("admin@123");
+		loginPOM.sendUserName("reva");
+		loginPOM.sendPassword("reva321");
 		loginPOM.clickLoginBtn(); 
 		screenShot.captureScreenShot("First");
+	}
+	
+	@Test
+	public void Logout()
+	{
+		GenericMethods gm= new GenericMethods(driver);
+		
+		loginPOM.moveToUserIcon(Usericon, UsericonClick);
+
+		
 	}
 }
