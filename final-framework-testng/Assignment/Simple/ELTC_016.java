c4012acd2f3db781045c94b64c23db8480d2847d
YEPURIRAO <YEPURIRAO@9.85.114.97>
2019-11-12 12:53:22 +0530

    done some chnages

--- final-framework-testng/Assignment/Simple/ELTC_016.java
+++ final-framework-testng/Assignment/Simple/ELTC_016.java
@@ -1,12 +1,24 @@
 package Simple;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-@Test
+
 public class ELTC_016
 {
-WebDriver driver= new ChromeDriver();
+	@Test
+	public void Login()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.navigate().to("http://elearningm1.upskills.in/");
+		driver.quit();
+	}
+
+
 
 }
